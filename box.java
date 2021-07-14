2fdf8c7a4794efbf7e77268221449b62107233e5
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-07-14 16:20:58 +0300

    param ton changed

--- box.java
+++ box.java
@@ -1,41 +1,20 @@
 class param{
-    double width,height,depth;
-    param(param ob){
-        width = ob.width;
-        height = ob.height;
-        depth = ob.depth;
-    }
-    param(double w ,double h, double d){
-        width = w;
-        height = h;
-        depth = d ;
-    }
-    param(){
-        width = -1;
-        height = -1;
-        depth = -1;
-
-    }
-    param(double len){
-        width = height = depth = len;
-    }
-    double volume(){
-        return width * height * depth;
+    void meth(int i, int j){
+        i*= 2;
+        j/= 2;
     }
 }
 
 
 public class box {
     public static void main(String args[]){
-        param mybox1 = new param(10,20,15);
-        param mybox2 = new param();
-        param mybox3 = new param(7);
-        param myclone = new param(mybox1);
-        
-        System.out.println("Volume one = "+ mybox1.volume());
-        System.out.println("Volume two = "+ mybox2.volume());
-        System.out.println("Volume cube = "+ mybox3.volume());
-        System.out.println("Volume clone = "+ myclone.volume());
+        param ob = new param();
+        int a = 15, b=20;
+
+        System.out.println("a and b before: " + a +" " + b);
+        ob.meth(a,b);
+
+        System.out.println("a and b after: " + a +" " + b );
     }
     
 }
