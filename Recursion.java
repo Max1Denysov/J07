e54a5aa523efa34eaf26d6a83457e7fc942da9d3
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-07-14 17:52:58 +0300

    recurs

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,22 @@
 class Factorial{
-    int fact(int n){
-        int result;
-        if (n==1) return 1;
-        result = fact(n-1) * n;
-        return result;
-
+    int values[];
+    Factorial(int i) {
+        values = new int[10];
+    }
+    void printArray(int i){
+        if (i==0) return;
+        else printArray(i-1);
+        System.out.println("[" + (i-1) +"]" + values[i-1] );
     }
 }
 
 
-public class Recursion {
+class Recursion {
     public static void main(String args[]){
-        Factorial f = new Factorial();
-        System.out.println("Factorial 125 is = " + f.fact(32) );
+        Factorial ob = new Factorial(10);
+        int i;
+        for(i = 0; i < 10; i++) ob.values[i] = i;
+        ob.printArray(10);
     }
     
 }
