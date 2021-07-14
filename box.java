42b78dcddb17209091802e1ed4cab0eed1b70f6e
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-07-14 16:45:35 +0300

    object changed paraneters by void

--- box.java
+++ box.java
@@ -1,20 +1,25 @@
 class param{
-    void meth(int i, int j){
-        i*= 2;
-        j/= 2;
+    int a,b;
+    param(int i , int j){
+        a = i ;
+        b = j;
+    }
+    void meth(param ob){
+        ob.a*= 2;
+        ob.b/= 2;
     }
 }
 
 
 public class box {
     public static void main(String args[]){
-        param ob = new param();
-        int a = 15, b=20;
+        param ob = new param(15,20);
+        
 
-        System.out.println("a and b before: " + a +" " + b);
-        ob.meth(a,b);
+        System.out.println("a and b before: " + ob.a +" " + ob.b);
+        ob.meth(ob);
 
-        System.out.println("a and b after: " + a +" " + b );
+        System.out.println("a and b after: " + ob.a +" " + ob.b );
     }
     
 }
